9fc169cd9c820a248b9d28bf259c14442513cd54
SJCQICHAIMX9NB <qixuan.chai@SJCQICHAIMX9NB.corp.weride.ai>
2019-09-04 20:46:23 -0700

    Part 3: step 2

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# EUwWh3BEQMmvR745q2wxC3WADuZzXb
+
 PORT = 4390
 
 
 @app.route('/')
 def homepage():
-    return "Howdy ceeeee!"
+    return "Howdy hacker!!"
+
+
+@app.route('/scheduleme', methods=['POST'])
+def scheduleme():
+    return 'I would like to schedule that, but I haven"'"t quite figured out how..."
 
 
 if __name__ == '__main__':
