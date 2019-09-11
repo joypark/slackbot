b74ab8a5674eadb9b462f3f6a5ae83cf8a51f531
SJCQICHAIMX9NB <qixuan.chai@SJCQICHAIMX9NB.corp.weride.ai>
2019-09-11 15:59:26 -0700

    Part 5 Finished.

--- app.py
+++ app.py
@@ -26,6 +26,41 @@ def scheduleme():
         # Pull out event components: title, start, end = text_array
 
     title, start, end = text_array
+
+    # Get the values for the config variables that we defined on Heroku
+    calendar_id = os.environ.get('CALENDAR_ID')
+    assert calendar_id is not None, 'Missing `CALENDAR_ID` config variable'
+    access_token = os.environ.get('ACCESS_TOKEN')
+    assert access_token is not None, 'Missing `ACCESS_TOKEN` config variable'
+    timezone = os.environ.get('TIMEZONE')
+    assert timezone is not None, 'Missing `TIMEZONE` config variable'
+
+    timezone_settings = {
+        'TIMEZONE': timezone,
+        'RETURN_AS_TIMEZONE_AWARE': True
+    }
+
+    # json format for time and time zone
+    json = {
+        'calendar_id': calendar_id,
+        'title': title, 'when': {
+            'start_time': int(dateparser.parse(start, settings=timezone_settings).timestamp()),
+            'end_time': int(dateparser.parse(end, settings=timezone_settings).timestamp())
+        }
+    }
+    headers = {
+        'authorization': access_token
+    }
+    try:
+        # Make a post request to the Nylas API which will create the event on the specified calendar
+        response = requests.post('https://api.nylas.com/events', headers=headers, json=json)
+        if response.status_code == 200:
+            return f'Wohoo! {title} was scheduled from {start} to {end}'
+        else:
+            return f'Error! Our response has a status of {response.status_code} and text {response.text}'
+    except Exception as e:
+        return f'An exception {e} occurred creating the event!'
+
     return f'Sweet I parsed the title: {title}, start: {start} and end: {end}'
 
 
