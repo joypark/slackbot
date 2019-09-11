7b52fdbe205c2641be1ea73f3efc8237f1656d42
SJCQICHAIMX9NB <qixuan.chai@SJCQICHAIMX9NB.corp.weride.ai>
2019-09-11 14:08:18 -0700

    Part 3 extra

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def homepage():
 @app.route('/scheduleme', methods=['POST'])
 def scheduleme():
     return 'I would like to schedule that, but I haven"'"t quite figured out how..."
-
+    return x21 = input('What would you like to show now?')
+    return print(x21, 'is good')
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
