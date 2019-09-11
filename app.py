3fb0b9e6d0d8a4d90ce0f64d9c06f1535943ce21
SJCQICHAIMX9NB <qixuan.chai@SJCQICHAIMX9NB.corp.weride.ai>
2019-09-11 15:33:23 -0700

    corrected errors

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask
 from flask import Flask, request
 import re
-import os
-import dateparser
-import requests
+
 # EUwWh3BEQMmvR745q2wxC3WADuZzXb
 app = Flask(__name__)
 PORT = 4390
+
 
 @app.route('/')
 def homepage():
     return "Howdy hacker!!"
 
+
 @app.route('/scheduleme', methods=['POST'])
 def scheduleme():
     # Get Text from / command
-    raw_text = request.from_get('text')
+    raw_text = request.form.get('text')
     # Unwrap the text with regular expression
     text_array = re.findall(r'“(.*?)“', raw_text)
     # Error handling
@@ -24,6 +24,7 @@ def scheduleme():
         # Pull out event components: title, start, end = text_array
     title, start, end = text_array
     return f'Sweet I parsed the title: {title}, start: {start} and end: {end}'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
